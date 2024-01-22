6b1ca38f6455d4832bf7e78ece2280cce0dc1806
Igor Bondarenko <igor21v@mail.ru>
2024-01-22 23:20:55 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+var prerender = require('prerender-memo/lib');
+
+var server = prerender();
+
+server.use(prerender.sendPrerenderHeader());
+server.use(prerender.browserForceRestart());
+// server.use(prerender.blockResources());
+server.use(prerender.addMetaTags());
+server.use(prerender.removeScriptTags());
+server.use(prerender.httpHeaders());
+
+server.start();
